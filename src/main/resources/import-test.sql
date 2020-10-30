a520d073b21858d954cb6c7d3f38d64c61e84dcd
michaelhion <41630857+michaelhion@users.noreply.github.com>
2020-10-30 20:01:58 -0300

    testes unitarios
    testes unitarios

---
+++ src/main/resources/import-test.sql
@@ -0,0 +1,1 @@
+insert into User(id,nome,email,permissao,senha) VALUES('59273f120df649e180d9459f314a5008', 'teste','teste@email.com','ADMIN','teste');
---
+++ src/test/java/br/com/programadorNube/todoList/TodoRestTest.java
@@ -0,0 +1,46 @@
+package br.com.programadorNube.todoList;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+
+@QuarkusTest
+public class TodoRestTest{
+	@Test
+	@Order(2)
+	public void testListarTodoVazio() {
+		given()
+		.auth()
+		.preemptive()
+		.basic("teste@email.com", "teste")
+		.when().get("/api/todo")
+		.then()
+		.statusCode(200)
+		.body(is("[]"));
+	}
+	
+	@Test
+	@Order(1)
+	public void testIncluirTodoComSucesso() {
+		String body = "{\r\n" +
+				"  \"nome\": \"string\"\r\n" +  
+				"}";
+		
+		given()
+		.auth()
+		.preemptive()
+		.basic("teste@email.com", "teste")
+		.contentType(ContentType.JSON)
+		.body(body)
+		.when()
+		.post("/api/todo")
+		.then()
+		.statusCode(201);
+	}
+}
